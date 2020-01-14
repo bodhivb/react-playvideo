3c19bf59cd9a1dd7b9f3ef0af25df27b89891841
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2020-01-14 11:43:01 +0100

    Added media interface and improvement url in videoplayer

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -2,6 +2,7 @@ import React, { PureComponent, ReactNode } from "react";
 
 interface IProps {
   src: string;
+
   width?: number | string;
   height?: number | string;
 
@@ -11,23 +12,81 @@ interface IProps {
   muted?: boolean;
 }
 
+type playerType = "iFrame" | "htmlVideo";
+
+interface IMedia {
+  url: string;
+  type: playerType;
+}
+
 export class Video extends PureComponent<IProps> {
   static defaultProps = {
     controls: true
   };
 
-  //Render iframe video (youtube or vimeo)
+  private convertToMedia(src: string): IMedia {
+    //YouTube
+    if (src.indexOf("youtu") >= 0) {
+      const fullUrl = src
+        .replace("watch?v=", "")
+        .replace("youtu.be", "youtube.com");
+
+      return {
+        url:
+          fullUrl.indexOf("embed") < 0
+            ? fullUrl.replace("youtube.com", "youtube.com/embed")
+            : fullUrl,
+        type: "iFrame"
+      };
+    }
+
+    //Vimeo
+    if (src.indexOf("vimeo") >= 0) {
+      return {
+        url:
+          src.indexOf("player") >= 0 || src.indexOf("video") >= 0
+            ? src
+            : src.replace("vimeo.com", "player.vimeo.com/video"),
+        type: "iFrame"
+      };
+    }
+
+    //Other videoplayer
+    return { url: src, type: "htmlVideo" };
+  }
+
+  //Render videoplayer
   public render(): ReactNode {
-    const { src, autoPlay, controls, width, height } = this.props;
-    return (
-      <iframe
-        src={src + (controls ? "" : "?controls=0")}
-        width={width}
-        height={height}
-        allow={"encrypted-media; " + autoPlay ? "autoplay" : ""}
-        allowFullScreen={true}
-        style={{ border: "none" }}
-      />
-    );
+    const media: IMedia = this.convertToMedia(this.props.src);
+
+    const { autoPlay, controls, width, height, loop, muted } = this.props;
+
+    if (media.type === "iFrame") {
+      return (
+        <iframe
+          src={media.url + (controls ? "" : "?controls=0")}
+          width={width}
+          height={height}
+          allow={"encrypted-media; " + autoPlay ? "autoplay" : ""}
+          allowFullScreen={true}
+          style={{ border: "none" }}
+        />
+      );
+    } else {
+      return (
+        <video
+          width={width}
+          height={height}
+          autoPlay={autoPlay}
+          loop={loop}
+          muted={muted}
+          controls={controls}
+          playsInline
+        >
+          <source src={media.url} type="video/mp4" />
+          Your browser does not support the videoplayer.
+        </video>
+      );
+    }
   }
 }
